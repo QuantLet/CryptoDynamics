20a7cf455f5c54add459c23b7152d7b72be99458
Boyuan Ning <73546421+ningboyuan@users.noreply.github.com>
2022-03-13 03:01:09 +0100

    Update CryptoDynamics_Series.R

--- CryptoDynamics_Series/CryptoDynamics_Series.R
+++ CryptoDynamics_Series/CryptoDynamics_Series.R
@@ -19,5 +19,5 @@ plot(data[,3]~date,type="l",ylim=c(-5,10),xlab="",ylab="",lwd=2)
 lines(data[,4]~date,col="blue",lwd=2)
 lines(data[,5]~date,col="red",lwd=2)
 lines(data[,6]~date,col="green",lwd=2)
-for(i in 6:20){lines(data[,i]~date,col="grey")}
+for(i in 6:12){lines(data[,i]~date,col="grey")}
 dev.off()
